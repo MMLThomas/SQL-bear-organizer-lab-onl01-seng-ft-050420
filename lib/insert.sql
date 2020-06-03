05314136428a35985f89416853d8772423ef1e2b
michael thomas <michael.thomas2014@gmail.com>
2020-06-03 06:01:19 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, 'Mr.Chocolate', NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, 'Rowdy', NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, 'Tabitha', NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, 'Sergeant Brown', NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, 'Melissa', NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, 'Grinch', NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, 'Windy', NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 20, "M", "black", "aggressive", 0);
